4ce0de8a130d76daf91574f6d5cec86c7d14fc8a
ichen-clmke <80910331+ichen-clmke@users.noreply.github.com>
2022-03-08 14:19:26 -0600

    Converted the GCP stuff to stuff we can use

--- startup.sh
+++ startup.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
-# Version 1.4.3
-# This is a startup script for UniFi Controller on Debian based Google Compute Engine instances.
-# For instructions and how-to:  https://metis.fi/en/2018/02/unifi-on-gcp/
-# For comments and code walkthrough:  https://metis.fi/en/2018/02/gcp-unifi-code/
+# Version 1.4.3.a
+# Forked from https://github.com/riihikallio/unifi
+# Original Script created by Petri Riihikallio https://metis.fi/en/petri-en/
+# Original Copyright (c) 2018, Petri Riihikallio, Metis Oy
+# Modified for use in the Cyberlynk Networks hosting environment by CyberLynk Networks
 #
-# You may use this as you see fit as long as I am credited for my work.
-# (c) 2018-2021 Petri Riihikallio Metis Oy
+# Comments regarding CyberLynk modifications are indicated with "CYB:"
 
 ###########################################################
 #
 # Set up logging for unattended scripts and UniFi's MongoDB log
 # Variables $LOG and $MONGOLOG are used later on in the script.
 #
-LOG="/var/log/unifi/gcp-unifi.log"
-if [ ! -f /etc/logrotate.d/gcp-unifi.conf ]; then
-	cat > /etc/logrotate.d/gcp-unifi.conf <<_EOF
+LOG="/var/log/unifi/cyb-unifi.log"
+if [ ! -f /etc/logrotate.d/cyb-unifi.conf ]; then
+	cat > /etc/logrotate.d/cyb-unifi.conf <<_EOF
 $LOG {
 	monthly
 	rotate 4
 	compress
 }
 _EOF
-	echo "Script logrotate set up"
+    echo "Script logrotate set up"
 fi
 
 MONGOLOG="/usr/lib/unifi/logs/mongod.log"
@@ -38,7 +38,7 @@ $MONGOLOG {
 	missingok
 }
 _EOF
-	echo "MongoDB logrotate set up"
+    echo "MongoDB logrotate set up"
 fi
 
 ###########################################################
@@ -46,38 +46,39 @@ fi
 # Turn off IPv6 for now
 #
 if [ ! -f /etc/sysctl.d/20-disableIPv6.conf ]; then
-	echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.d/20-disableIPv6.conf
-	sysctl --system > /dev/null
-	echo "IPv6 disabled"
+    echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.d/20-disableIPv6.conf
+    sysctl --system > /dev/null
+    echo "IPv6 disabled"
 fi
 
 ###########################################################
 #
 # Update DynDNS as early in the script as possible
+# CYB: Not applicable for our use case
 #
-ddns=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ddns-url")
-if [ ${ddns} ]; then
-	curl -fs ${ddns}
-	echo "Dynamic DNS accessed"
-fi
+# ddns=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ddns-url")
+# if [ ${ddns} ]; then
+# 	curl -fs ${ddns}
+#	echo "Dynamic DNS accessed"
+# fi
 
 ###########################################################
 #
 # Create a swap file for small memory instances and increase /run
 #
 if [ ! -f /swapfile ]; then
-	memory=$(free -m | grep "^Mem:" | tr -s " " | cut -d " " -f 2)
-	echo "${memory} megabytes of memory detected"
-	if [ -z ${memory} ] || [ "0${memory}" -lt "2048" ]; then
-		fallocate -l 2G /swapfile
-		chmod 600 /swapfile
-		mkswap /swapfile >/dev/null
-		swapon /swapfile
-		echo '/swapfile none swap sw 0 0' >> /etc/fstab
-		echo 'tmpfs /run tmpfs rw,nodev,nosuid,size=400M 0 0' >> /etc/fstab
-		mount -o remount,rw,nodev,nosuid,size=400M tmpfs /run
-		echo "Swap file created"
-	fi
+    memory=$(free -m | grep "^Mem:" | tr -s " " | cut -d " " -f 2)
+    echo "${memory} megabytes of memory detected"
+    if [ -z ${memory} ] || [ "0${memory}" -lt "2048" ]; then
+        fallocate -l 2G /swapfile
+        chmod 600 /swapfile
+        mkswap /swapfile >/dev/null
+        swapon /swapfile
+        echo '/swapfile none swap sw 0 0' >> /etc/fstab
+        echo 'tmpfs /run tmpfs rw,nodev,nosuid,size=400M 0 0' >> /etc/fstab
+        mount -o remount,rw,nodev,nosuid,size=400M tmpfs /run
+        echo "Swap file created"
+    fi
 fi
 
 ###########################################################
@@ -90,7 +91,7 @@ if [ ${release} ] && [ ! -f /etc/apt/sources.list.d/backports.list ]; then
 deb http://deb.debian.org/debian/ ${release}-backports main
 deb-src http://deb.debian.org/debian/ ${release}-backports main
 _EOF
-	echo "Backports (${release}) added to APT sources"
+    echo "Backports (${release}) added to APT sources"
 fi
 
 ###########################################################
@@ -99,52 +100,53 @@ fi
 #
 
 # Required preliminiaries
-if [ ! -f /usr/share/misc/apt-upgraded-1 ]; then
-	export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn    # For CGP packages
-	curl -Lfs https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -    # For CGP packages
-	apt-get -qq update -y >/dev/null
-	DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y >/dev/null    # GRUB upgrades require special flags
-	rm /usr/share/misc/apt-upgraded    # Old flag file
-	touch /usr/share/misc/apt-upgraded-1
-	echo "System upgraded"
-fi
+# CYB: Doesn't look applicable, will try it without and report back
+# if [ ! -f /usr/share/misc/apt-upgraded-1 ]; then
+# 	export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn    # For CGP packages
+# 	curl -Lfs https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -    # For CGP packages
+# 	apt-get -qq update -y >/dev/null
+# 	DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y >/dev/null    # GRUB upgrades require special flags
+# 	rm /usr/share/misc/apt-upgraded    # Old flag file
+# 	touch /usr/share/misc/apt-upgraded-1
+# 	echo "System upgraded"
+# fi
 
 # HAVEGEd is straightforward
 haveged=$(dpkg-query -W --showformat='${Status}\n' haveged 2>/dev/null)
-if [ "x${haveged}" != "xinstall ok installed" ]; then 
-	if apt-get -qq install -y haveged >/dev/null; then
-		echo "Haveged installed"
-	fi
+if [ "x${haveged}" != "xinstall ok installed" ]; then
+    if apt-get -qq install -y haveged >/dev/null; then
+        echo "Haveged installed"
+    fi
 fi
 certbot=$(dpkg-query -W --showformat='${Status}\n' certbot 2>/dev/null)
 if [ "x${certbot}" != "xinstall ok installed" ]; then
-if (apt-get -qq install -y -t ${release}-backports certbot >/dev/null) || (apt-get -qq install -y certbot >/dev/null); then
-		echo "CertBot installed"
-	fi
+    if (apt-get -qq install -y -t ${release}-backports certbot >/dev/null) || (apt-get -qq install -y certbot >/dev/null); then
+        echo "CertBot installed"
+    fi
 fi
 
 # UniFi needs https support, custom repo and APT update first
 apt-get -qq install -y apt-transport-https >/dev/null
 unifi=$(dpkg-query -W --showformat='${Status}\n' unifi 2>/dev/null)
 if [ "x${unifi}" != "xinstall ok installed" ]; then
-	echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" > /etc/apt/sources.list.d/unifi.list
-	curl -Lfs -o /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg
-	apt-get -qq update -y >/dev/null
-	
-	if apt-get -qq install -y openjdk-8-jre-headless >/dev/null; then
-		echo "Java 8 installed"
-	fi
-	if apt-get -qq install -y unifi >/dev/null; then
-		echo "Unifi installed"
-	fi
-	systemctl stop mongodb
-	systemctl disable mongodb
+    echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" > /etc/apt/sources.list.d/unifi.list
+    curl -Lfs -o /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg
+    apt-get -qq update -y >/dev/null
+    
+    if apt-get -qq install -y openjdk-8-jre-headless >/dev/null; then
+        echo "Java 8 installed"
+    fi
+    if apt-get -qq install -y unifi >/dev/null; then
+        echo "Unifi installed"
+    fi
+    systemctl stop mongodb
+    systemctl disable mongodb
 fi
 
 # Lighttpd needs a config file and a reload
 httpd=$(dpkg-query -W --showformat='${Status}\n' lighttpd 2>/dev/null)
 if [ "x${httpd}" != "xinstall ok installed" ]; then
-	if apt-get -qq install -y lighttpd >/dev/null; then
+    if apt-get -qq install -y lighttpd >/dev/null; then
 		cat > /etc/lighttpd/conf-enabled/10-unifi-redirect.conf <<_EOF
 \$HTTP["scheme"] == "http" {
     \$HTTP["host"] =~ ".*" {
@@ -152,18 +154,18 @@ if [ "x${httpd}" != "xinstall ok installed" ]; then
     }
 }
 _EOF
-		systemctl reload-or-restart lighttpd
-		echo "Lighttpd installed"
-	fi
+        systemctl reload-or-restart lighttpd
+        echo "Lighttpd installed"
+    fi
 fi
 
 # Fail2Ban needs three files and a reload
 f2b=$(dpkg-query -W --showformat='${Status}\n' fail2ban 2>/dev/null)
-if [ "x${f2b}" != "xinstall ok installed" ]; then 
-	if apt-get -qq install -y fail2ban >/dev/null; then
-			echo "Fail2Ban installed"
-	fi
-	if [ ! -f /etc/fail2ban/filter.d/unifi-controller.conf ]; then
+if [ "x${f2b}" != "xinstall ok installed" ]; then
+    if apt-get -qq install -y fail2ban >/dev/null; then
+        echo "Fail2Ban installed"
+    fi
+    if [ ! -f /etc/fail2ban/filter.d/unifi-controller.conf ]; then
 		cat > /etc/fail2ban/filter.d/unifi-controller.conf <<_EOF
 [Definition]
 failregex = ^.* Failed .* login for .* from <HOST>\s*$
@@ -174,8 +176,8 @@ filter   = unifi-controller
 port     = 8443
 logpath  = /var/log/unifi/server.log
 _EOF
-	fi
-	# The .local file will be installed in any case
+    fi
+    # The .local file will be installed in any case
 	cat > /etc/fail2ban/jail.d/unifi-controller.local <<_EOF
 [unifi-controller]
 enabled  = true
@@ -183,7 +185,7 @@ maxretry = 3
 bantime  = 3600
 findtime = 3600
 _EOF
-	systemctl reload-or-restart fail2ban
+    systemctl reload-or-restart fail2ban
 fi
 
 ###########################################################
@@ -197,17 +199,18 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 ###########################################################
 #
 # Set the time zone
+# CYB: Set permanently to America/Chicago
 #
-tz=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/timezone")
+tz="America/Chicago" #$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/timezone")
 if [ ${tz} ] && [ -f /usr/share/zoneinfo/${tz} ]; then
-	apt-get -qq install -y dbus >/dev/null
-	if ! systemctl start dbus; then
-		echo "Trying to start dbus"
-		sleep 15
-		systemctl start dbus
-	fi
-	if timedatectl set-timezone $tz; then echo "Localtime set to ${tz}"; fi
-	systemctl reload-or-restart rsyslog
+    apt-get -qq install -y dbus >/dev/null
+    if ! systemctl start dbus; then
+        echo "Trying to start dbus"
+        sleep 15
+        systemctl start dbus
+    fi
+    if timedatectl set-timezone $tz; then echo "Localtime set to ${tz}"; fi
+    systemctl reload-or-restart rsyslog
 fi
 
 ###########################################################
@@ -224,7 +227,7 @@ Unattended-Upgrade::Origins-Pattern {
 Unattended-Upgrade::Remove-Unused-Dependencies "true";
 Unattended-Upgrade::Automatic-Reboot "true";
 _EOF
-
+    
 	cat > /etc/systemd/system/timers.target.wants/apt-daily-upgrade.timer <<_EOF
 [Unit]
 Description=Daily apt upgrade and clean activities
@@ -236,9 +239,9 @@ Persistent=true
 [Install]
 WantedBy=timers.target
 _EOF
-	systemctl daemon-reload
-	systemctl reload-or-restart unattended-upgrades
-	echo "Unattended upgrades set up"
+    systemctl daemon-reload
+    systemctl reload-or-restart unattended-upgrades
+    echo "Unattended upgrades set up"
 fi
 
 ###########################################################
@@ -267,8 +270,8 @@ else
 fi
 exit 0
 _EOF
-	chmod a+x /usr/local/sbin/unifidb-repair.sh
-
+    chmod a+x /usr/local/sbin/unifidb-repair.sh
+    
 	cat > /etc/systemd/system/unifidb-repair.service <<_EOF
 [Unit]
 Description=Repair UniFi MongoDB database at boot
@@ -281,39 +284,40 @@ ExecStart=/usr/local/sbin/unifidb-repair.sh
 [Install]
 WantedBy=multi-user.target
 _EOF
-	systemctl enable unifidb-repair.service
-	echo "Unifi DB autorepair set up"
+    systemctl enable unifidb-repair.service
+    echo "Unifi DB autorepair set up"
 fi
 
 ###########################################################
 #
 # Set up daily backup to a bucket after 01:00
+# CYB: We are a doing a full VM backup already.  No need for this
 #
-bucket=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/bucket")
-if [ ${bucket} ]; then
-	cat > /etc/systemd/system/unifi-backup.service <<_EOF
-[Unit]
-Description=Daily backup to ${bucket} service
-After=network-online.target
-Wants=network-online.target
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/gsutil rsync -r -d /var/lib/unifi/backup gs://$bucket
-_EOF
-
-	cat > /etc/systemd/system/unifi-backup.timer <<_EOF
-[Unit]
-Description=Daily backup to ${bucket} timer
-[Timer]
-OnCalendar=1:00
-RandomizedDelaySec=30m
-[Install]
-WantedBy=timers.target
-_EOF
-	systemctl daemon-reload
-	systemctl start unifi-backup.timer
-	echo "Backups to ${bucket} set up"
-fi
+# bucket=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/bucket")
+# if [ ${bucket} ]; then
+# 	cat > /etc/systemd/system/unifi-backup.service <<_EOF
+# [Unit]
+# Description=Daily backup to ${bucket} service
+# After=network-online.target
+# Wants=network-online.target
+# [Service]
+# Type=oneshot
+# ExecStart=/usr/bin/gsutil rsync -r -d /var/lib/unifi/backup gs://$bucket
+# _EOF
+#
+# 	cat > /etc/systemd/system/unifi-backup.timer <<_EOF
+# [Unit]
+# Description=Daily backup to ${bucket} timer
+# [Timer]
+# OnCalendar=1:00
+# RandomizedDelaySec=30m
+# [Install]
+# WantedBy=timers.target
+# _EOF
+# 	systemctl daemon-reload
+# 	systemctl start unifi-backup.timer
+# 	echo "Backups to ${bucket} set up"
+# fi
 
 ###########################################################
 #
@@ -331,7 +335,7 @@ fi
 #	 	echo "unifi.xms=${xms}" >>/var/lib/unifi/system.properties
 #	 fi
 #	 message=" xms=${xms}"
-#	 
+#
 #	 if [ "0${xmx}" -lt "${xms}" ]; then xmx=${xms}; fi
 #	 if grep -e "^\s*unifi.xmx=[0-9]" /var/lib/unifi/system.properties >/dev/null; then
 #	 	sed -i -e "s/^[[:space:]]*unifi.xmx=[[:digit:]]\+/unifi.xmx=${xmx}/" /var/lib/unifi/system.properties
@@ -339,7 +343,7 @@ fi
 #	 	echo "unifi.xmx=${xmx}" >>/var/lib/unifi/system.properties
 #	 fi
 #	 message="${message} xmx=${xmx}"
-#	 
+#
 #	 if [ -n "${message}" ]; then
 #	 	echo "Java heap set to:${message}"
 #	 fi
@@ -351,7 +355,10 @@ fi
 #
 # Set up Let's Encrypt
 #
-dnsname=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/dns-name")
+# CYB: Let's make ours user interactive for now
+echo enter FQDN
+read -r dnsname
+# dnsname=$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/dns-name")
 if [ -z ${dnsname} ]; then exit 0; fi
 privkey=/etc/letsencrypt/live/${dnsname}/privkey.pem
 pubcrt=/etc/letsencrypt/live/${dnsname}/cert.pem
@@ -395,7 +402,7 @@ _EOF
 
 # Write pre and post hooks to stop Lighttpd for the renewal
 if [ ! -d /etc/letsencrypt/renewal-hooks/pre ]; then
-	mkdir -p /etc/letsencrypt/renewal-hooks/pre
+    mkdir -p /etc/letsencrypt/renewal-hooks/pre
 fi
 cat > /etc/letsencrypt/renewal-hooks/pre/lighttpd <<_EOF
 #! /bin/sh
@@ -404,7 +411,7 @@ _EOF
 chmod a+x /etc/letsencrypt/renewal-hooks/pre/lighttpd
 
 if [ ! -d /etc/letsencrypt/renewal-hooks/post ]; then
-	mkdir -p /etc/letsencrypt/renewal-hooks/post
+    mkdir -p /etc/letsencrypt/renewal-hooks/post
 fi
 cat > /etc/letsencrypt/renewal-hooks/post/lighttpd <<_EOF
 #! /bin/sh
@@ -414,7 +421,7 @@ chmod a+x /etc/letsencrypt/renewal-hooks/post/lighttpd
 
 # Write the deploy hook to import the cert into Java
 if [ ! -d /etc/letsencrypt/renewal-hooks/deploy ]; then
-	mkdir -p /etc/letsencrypt/renewal-hooks/deploy
+    mkdir -p /etc/letsencrypt/renewal-hooks/deploy
 fi
 cat > /etc/letsencrypt/renewal-hooks/deploy/unifi <<_EOF
 #! /bin/bash
@@ -423,11 +430,11 @@ if [ -e $privkey ] && [ -e $pubcrt ] && [ -e $chain ]; then
 
 	echo >> $LOG
 	echo "Importing new certificate on \$(date)" >> $LOG
-	
+
 	p12=\$(mktemp)
 	combo=\$(mktemp)
 	cat $pubcrt <(echo) $chain <(echo) $chroot > \${combo}
-	
+
 	if ! openssl pkcs12 -export \\
 	-in \${combo} \\
 	-inkey $privkey \\
@@ -437,13 +444,13 @@ if [ -e $privkey ] && [ -e $pubcrt ] && [ -e $chain ]; then
 		echo "OpenSSL export failed" >> $LOG
 		exit 1
 	fi
-	
+
 	if ! keytool -delete -alias unifi \\
 	-keystore /var/lib/unifi/keystore \\
 	-deststorepass aircontrolenterprise >/dev/null ; then
 		echo "KeyTool delete failed" >> $LOG
 	fi
-	
+
 	if ! keytool -importkeystore \\
 	-srckeystore \${p12} \\
 	-srcstoretype pkcs12 \\
@@ -455,7 +462,7 @@ if [ -e $privkey ] && [ -e $pubcrt ] && [ -e $chain ]; then
 		echo "KeyTool import failed" >> $LOG
 		exit 2
 	fi
-	
+
 	systemctl stop unifi
 	if ! java -jar /usr/lib/unifi/lib/ace.jar import_cert \\
 	$pubcrt $chain $caroot >/dev/null; then
@@ -477,7 +484,9 @@ chmod a+x /etc/letsencrypt/renewal-hooks/deploy/unifi
 # Write a script to acquire the first certificate (for a systemd timer)
 cat > /usr/local/sbin/certbotrun.sh <<_EOF
 #! /bin/sh
-extIP=\$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")
+# e#xtIP=\$(curl -fs -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")
+# CYB: The above won't work for us, using ifconfig.me
+extIP=\$(curl ifconfig.me)
 dnsIP=\$(getent hosts ${dnsname} | cut -d " " -f 1)
 
 echo >> $LOG
@@ -511,8 +520,8 @@ RandomizedDelaySec=15m
 [Install]
 WantedBy=timers.target
 _EOF
-	systemctl daemon-reload
-
+    systemctl daemon-reload
+    
 	cat > /etc/systemd/system/certbotrun.service <<_EOF
 [Unit]
 Description=Run CertBot hourly until success
@@ -526,9 +535,9 @@ fi
 
 # Start the above
 if [ ! -d /etc/letsencrypt/live/${dnsname} ]; then
-	if ! /usr/local/sbin/certbotrun.sh; then
-		echo "Installing hourly CertBot run"
-		systemctl start certbotrun.timer
-	fi
+    if ! /usr/local/sbin/certbotrun.sh; then
+        echo "Installing hourly CertBot run"
+        systemctl start certbotrun.timer
+    fi
 fi
 
